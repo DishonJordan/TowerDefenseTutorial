89b84cca8a4f35c9f819d962a54faeb5cfbdfc73
DishonJordan <dishonjordan@hotmail.com>
2019-04-24 23:44:15 -0700

    Reworked L movement script and renamed to MultiPointMovement

---
+++ Assets/Scripts/TowerDefense/Towers/Movement/MultiPointMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPointMovement : MonoBehaviour
+{
+    //Tower movement speed
+    public float speed;
+
+    //Defines that part of the traversal we are on
+    int segment;
+
+    //Holds an ordered set of points to be traversed 
+    public Vector3[] points = new Vector3[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializing the segments, start position
+        segment = 1;
+        transform.position = points[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, points[segment], Time.deltaTime * speed);
+        if (transform.position == points[segment])
+            segment = (segment == 3) ? 0 : segment + 1;
+
+    }
+
+}
