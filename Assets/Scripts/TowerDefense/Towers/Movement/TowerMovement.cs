64ab44444e627917cd7adea372dea6ea096c454b
DishonJordan <dishonjordan@hotmail.com>
2019-05-05 23:26:39 -0700

    Merged multi and single point movement scripts into a single TowerMovement script

--- Assets/Scripts/TowerDefense/Towers/Movement/TowerMovement.cs
+++ Assets/Scripts/TowerDefense/Towers/Movement/TowerMovement.cs
@@ -1,23 +1,23 @@
 ﻿using UnityEngine;
 
-public class MultiPointMovement : MonoBehaviour
+public class TowerMovement : MonoBehaviour
 {
-
     public float speed;
     private int currentPath;
-    public Vector3[] orderedPoints = new Vector3[4];
+    public Vector3[] orderedPoints;
 
     void Start()
     {
         currentPath = 1;
         transform.position = orderedPoints[0];
     }
+
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, orderedPoints[currentPath], Time.deltaTime * speed);
         if (transform.position == orderedPoints[currentPath])
         {
-            if (currentPath == 3)
+            if (currentPath == orderedPoints.Length - 1)
             {
                 currentPath = 0;
             }
